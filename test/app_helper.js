eb96bf6249b3d6f7190eb737ece5333ddd8520ba
Dmitry White <white.dmitry@gmail.com>
2014-09-08 20:35:07 -0700

    Installed Caribou build tools

---
+++ test/app_helper.js
@@ -0,0 +1,10 @@
+'use strict';
+
+
+
+// expose fixtures property (stored in __html__ via karma preprocessor)
+window.__fixture = function(name) {
+  var fixture = window.__html__['test/fixtures/' + name + '.json'];
+  if (!fixture) { throw new Error('Missing fixture ' + name); }
+  return JSON.parse(fixture);
+};
